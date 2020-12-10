b3ccc13b66cbd9378c8de1ec6cc3587326c60add
Pauline JOBERT <pauline.jobert31@gmail.com>
2020-12-10 14:17:08 +0100

    réglage pour compilation

---
+++ tests/reseau_test.c
@@ -0,0 +1,20 @@
+//#include <malloc.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+//#include "utils.h"
+#include "matrices.h"
+#include "matrices_struct.h"
+#include "reseau.h"
+
+
+
+int main() {
+
+    struct reseau_s *reseau = NULL;
+
+
+    return 0;
+}
