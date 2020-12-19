6d94de1a3f2df32499ba3bf3d6081a973a4b8196
Pauline JOBERT <pauline.jobert31@gmail.com>
2020-12-19 18:21:10 +0100

    modifications module reseau

--- src/reseau.c
+++ src/reseau.c
@@ -20,11 +20,12 @@
 void detruit_reseau ( struct reseau_s * r ){
 		//on fait une boucle qui parcourt toutes les couches du reseau en partant de la dernière
     for ( int i = r->nb_couches - 1 ; i >= 0  ; i-- ){ 
-		//Pour chaque couche, on appelle la fonction détruit qui se trouve dans la structure couche_s du reseau r (dans couches_struct.h)
+		//Pour chaque couche, on appelle la fonction détruit qui se trouve dans la structure couche_s du reseau r 
 		//Cela permet de détruire la struct couche_s sur laquelle pointe le réseau
 		r->couche[i]->detruit(r->couche[i]);
     }
-    //Ensuite on détruit le réseau
+    // aprés avoir détruit toutes les couches du réseau,
+    //on détruit le réseau
     free(r);
 }
 
@@ -40,7 +41,7 @@ struct matrice_s * reseau_get_entree ( struct reseau_s * r ){
 	// et donc on renvoie cette couche suivante en parcourant le chemin décrit ci-dessus pour y arriver
 	
 	//Mais avant tout il faut savoir quelle est la couche d'entrée, alors on parcourt toutes les couches du réseau,
-	// et on test le type jusqu'à ce qu'on tombe sur la couche d'entrée
+	// et on teste le type jusqu'à ce qu'on tombe sur la couche d'entrée
 	for (int i=0; i<r->nb_couches ; i++){
 		if ( r->couche[i]->t == 0){ //lorsque t=0 on est sur une couche d'entrée
 			return (r->couche[i]->activation.s) ;
@@ -54,13 +55,13 @@ struct matrice_s * reseau_get_entree ( struct reseau_s * r ){
  */
 struct matrice_s * reseau_get_sortie ( struct reseau_s * r ){
 	//On voit que la fonction réseau pointe sur une structure "couche_s"
-	//On choisi de pointer sur la couche de sortie qui est la couche d'indice "r->nb_couches-1"
+	//On choisi de pointer sur la couche de sortie
 	//Cette structure couche_s pointe sur une structure "inter_couche_s activation"
 	//Celle ci pointe sur "la couche précedente" notée "p" (de type "struct matrice_s *")
 	// et donc on renvoie cette couche précédente en parcourant le chemin décrit ci-dessus pour y arriver
 	
-	//Mais avant tout il faut savoir quelle est la couche d'entrée, alors on parcourt toutes les couches du réseau,
-	// et on test le type jusqu'à ce qu'on tombe sur la couche d'entrée
+	//Mais avant tout il faut savoir quelle est la couche de sortiee, alors on parcourt toutes les couches du réseau,
+	// et on teste le type jusqu'à ce qu'on tombe sur la couche d'entrée
 	for (int i=0; i<r->nb_couches ; i++){
 		if ( r->couche[i]->t == 1){ //lorsque t=1 on est sur une couche de sortie
 			return (r->couche[i]->activation.s );
@@ -75,7 +76,7 @@ struct matrice_s * reseau_get_sortie ( struct reseau_s * r ){
   la couche 0.
  */
 void propagation (struct matrice_s * entree , struct reseau_s * r ){
-	//on fait une boucle qui parcourt toutes les couches du reseau en partant de la dernière
+	//on fait une boucle qui parcourt toutes les couches du reseau en partant de la première
     for (int i=0; i< r->nb_couches; i++){
 		//Pour chaque couche, on appelle la fonction de propagation qui se trouve dans la structure couche_s du reseau r (dans couches_struct.h)
 		r->couche[i]->propagation (entree ,r->couche[i]);
@@ -134,8 +135,9 @@ static int lire_type_de_couche ( FILE * f , type_couche * t ) {
 /*
   Lit un réseau dans un fichier qui a été écrit par ecrit_reseau_fichier.
  */
- 
-//ecrit_reseau_fichier écrit le nombre de couches d'un réseau et le contenu de chacune des couches, nous allons donc les lires
+ //ecrit_reseau_fichier écrit le nombre de couches d'un réseau et le contenu de chacune des couches, nous allons donc les lires
+
+
 int lit_reseau_fichier ( FILE * f ,  struct reseau_s ** r ) {
     int i ;
     struct matrice_s * matricetmp;
@@ -152,7 +154,7 @@ int lit_reseau_fichier ( FILE * f ,  struct reseau_s ** r ) {
 	fscanf ( f , "%d\n" , &((*r)->nb_couches)) ; 
 	//on parcourt les couches 
     
-    //on verifie que la couche soit bien définie
+    //on verifie que le type couche soit bien définie
     if (lire_type_de_couche (f,&(*r)->couche[i]->t )==1){
         printf("le type de la couche est undéfini");
     } else {
@@ -162,9 +164,15 @@ int lit_reseau_fichier ( FILE * f ,  struct reseau_s ** r ) {
 		
 		    if (lire_mot_clef(f, "entree")== 0 ) { //la couche est de type couche_entree
 			    (*r)->couche[i]->t=type_couche_entree;
+                // on appelle la fonction  struct couche_s *couche_entree (couche_s * suivante )
+                //Je préfère le dire en commenatire plutot que de l'appeler car je ne sais pas comment elle se 
+                //comporte et je ne voudrais pas faire de bétise
 		    }
 		    if (lire_mot_clef(f, "sortie") == 0){//la couche est de type couche_sortie
 			    (*r)->couche[i]->t=type_couche_sortie;
+                // on appelle la fonction  struct couche_s *couche_sortie (int n )
+                //Je préfère le dire en commenatire plutot que de l'appeler car je ne sais pas comment elle se 
+                //comporte et je ne voudrais pas faire de bétise
 		    }
 		    if (lire_mot_clef(f, "matrice") == 0){//la couche est de type couche_matrice
 			    //on lit la matrice à l'aide de "lire_matrice_fichier"
@@ -172,6 +180,8 @@ int lit_reseau_fichier ( FILE * f ,  struct reseau_s ** r ) {
 			    (*r)->couche[i]->t=type_couche_matrice;
 			    lire_matrice_fichier (f ,&matricetmp );
 			    (*r)->couche[i]->data.m =matricetmp;
+                // je sais qu'il faut appeler struct couche_s *couche_matrice (int n , struct couche_s * suivante  ) ;
+                // mais je ne sais pas dans quelle condition
 		    }
 		    if (lire_mot_clef(f, "activation") == 0){//la couche est de type couche_activation
 			    //on lit la fonction d'activation à l'aide de "lit_fonction_d_activation"
@@ -180,6 +190,8 @@ int lit_reseau_fichier ( FILE * f ,  struct reseau_s ** r ) {
 		        funtmp=malloc(sizeof(struct fonction_d_activation_s));
 			    lit_fonction_d_activation (f ,&funtmp );
 			    (*r)->couche[i]->data.f =funtmp;
+                // je sais qu'il faut appeler struct couche_s * couche_activation (struct fonction_d_activation_s * fun ,struct couche_s * suivante ) ;
+                // mais je ne sais pas dans quelle condition
 		    }
 	    }
     }
